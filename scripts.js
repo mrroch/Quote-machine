9a0da98020d5167da27fccc1efe9a739a7848324
Michał <michalroch@wp.pl>
2017-07-25 18:54:32 +0200

    changes in js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,7 @@ var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(prefix + quoteUrl, createTweet);
-    $.ajaxSetup({
-        cache: false
-    });
+    $.getJSON(quoteUrl, createTweet);
 }
 
 function createTweet(input) {
@@ -19,7 +16,7 @@ function createTweet(input) {
         quoteAuthor = "Unknown author";
     }
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-    
+
     if (tweetText.length > 140) {
         getQuote();
     } else {
