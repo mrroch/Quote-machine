0e06030cc8509f99e9b4d0f87b659cd459ba747b
Michal Roch <michalroch@wp.pl>
2017-08-22 15:30:53 +0200

    changes

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
-var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
-    $.ajaxSetup({
-        cache: false
-    });
+	$.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
@@ -18,21 +16,22 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-    
-    if (tweetText.length > 140) {
-        getQuote();
-    } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-        $('.quote').text(quoteText);
-        $('.author').text("Author: " + quoteAuthor);
-        $('.tweet').attr('href', tweet);
-    }
-};
 
-$(document).ready(function () {
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+    	getQuote();
+	} else {
+    	var tweet = tweetLink + encodeURIComponent(tweetText);
+		    $('.quote').text(quoteText);
+		    $('.author').text("Author: " + quoteAuthor);
+		    $('.tweet').attr('href', tweet);
+	}
+}
+
+$(document).ready(function() {
     getQuote();
-    $('.trigger').click(function () {
+    $('.trigger').click(function() {
         getQuote();
     })
 });
